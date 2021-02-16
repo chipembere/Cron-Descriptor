9e128dc94aaed1f953823aa0625e524210fde358
admin <admin@INDXGBR043.local>
2021-02-16 15:08:03 +0300

    to_datetime return type test.

--- cron-dis/test_cron_reader.py
+++ cron-dis/test_cron_reader.py
@@ -1,12 +1,11 @@
 import unittest
+import datetime
+from cron_reader import to_datetime
 
 class TestSum(unittest.TestCase):
 
-    # def test_sum(self):
-    #     self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    # def test_sum_tuple(self):
-    #     self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+    def test_to_datetime(self):
+        self.assertEqual(type(to_datetime("10:30")), datetime.time, "Should datetime 'time' object")
 
 if __name__ == '__main__':
     unittest.main()
